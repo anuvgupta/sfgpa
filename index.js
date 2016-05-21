2d8fc2d8335c148ada34b3335b20cf056fd31ddd
Anuv Gupta <anuv.ca@gmail.com>
2016-05-21 01:10:11 -0700

    test

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var tools = ['home', 'gpa'];
 
 function resize() {
 	if (window.innerWidth < window.innerHeight) {
-		_('menu').style.height = '13%';
-		_('mainTop').style.height = '13%';
+		
 	}
 	else {
 		_('menu').style.height = '73px';
@@ -20,12 +19,12 @@ function lClickHandler(j) {
 		});
 		$('head').append("<link rel = 'stylesheet' type = 'text/css' href = './tools/" + tools[j - 1] + "/" + tools[j - 1] + ".css'>");
 		$.getScript('./tools/' + tools[j - 1] + '/' + tools[j - 1] + '.js');
-		$('head').append("<meta name = 'viewport' content = 'width=device-width, initial-scale=1.8, maximum-scale=1.8, user-scalable=no'/>");
+		//$('head').append("<meta name = 'viewport' content = 'width=device-width, initial-scale=1.8, maximum-scale=1.8, user-scalable=no'/>");
 	};
 }
 
 function setEvents() {
-	for (var i = 1; i <= 3; i++) $('#l' + i).click(lClickHandler(i));
+	for (var i = 1; i <= tools.length; i++) $('#l' + i).click(lClickHandler(i));
 }
 
 function load() {
