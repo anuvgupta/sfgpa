31db28de37a58534873ef0590ec1838b6d3c64d8
Anuv Gupta <anuv.ca@gmail.com>
2016-05-20 21:18:21 -0700

    added scaling

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function resize() {
+	if (window.innerWidth < window.innerHeight) _('menu').style.height = '13%';
+	else _('menu').style.height = '73px';
+}
+
+$(window).resize(resize);
+$(document).ready(resize);
