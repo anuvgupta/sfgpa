ee65c32e822246d76a9997e22683127f069ce72a
Anuv Gupta <anuv.ca@gmail.com>
2016-05-27 18:28:47 -0700

    scaling

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function load() {
 	$('head').append("<link rel = 'stylesheet' type = 'text/css' href = './tools/" + tools[0] + "/" + tools[0] + ".css'>");
 	$.getScript('./tools/' + tools[0] + '/' + tools[0] + '.js');
 	if (mobile()) {
-		if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) viewport.setAttribute("content", "initial-scale=1.3, maximum-scale=1.3");
+		if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) viewport.setAttribute("content", "initial-scale=1.3, maximum-scale=1.3, user-scalable=no");
 		else viewport.setAttribute("content", "initial-scale=1, maximum-scale=1, user-scalable=no");
 	}
 }
