bc84c33b81b46a71dcd77380cd287d96a22a1182
Anuv Gupta <anuv.ca@gmail.com>
2016-05-27 18:40:47 -0700

    added final grade calculator files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var tools = ['home', 'gpa'];
+var tools = ['home', 'gpa', 'final'];
 
 function lClickHandler(j) {
 	return function(event) {
 		$('.linkTD').css('backgroundColor', '');
-		if (j > 1) _('l' + j).style.backgroundColor = '#BFBFBF';
+		if (j > 1) _('l' + j).style.backgroundColor = '#C0C0C0';
 		$.get('./tools/' + tools[j - 1] + '/' + tools[j - 1] + '.html', function(data) {
     		_('mainContent').innerHTML = data;
 		});
@@ -17,16 +17,16 @@ function setEvents() {
 }
 
 function load() {
+	if (mobile()) {
+		if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) viewport.setAttribute("content", "initial-scale=1.3, maximum-scale=1.3, user-scalable=no");
+		else viewport.setAttribute("content", "initial-scale=1, maximum-scale=1, user-scalable=no");
+	}
 	setEvents();
 	$.get('./tools/' + tools[0] + '/' + tools[0] + '.html', function(data) {
     	_('mainContent').innerHTML = data;
 	});
 	$('head').append("<link rel = 'stylesheet' type = 'text/css' href = './tools/" + tools[0] + "/" + tools[0] + ".css'>");
 	$.getScript('./tools/' + tools[0] + '/' + tools[0] + '.js');
-	if (mobile()) {
-		if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) viewport.setAttribute("content", "initial-scale=1.3, maximum-scale=1.3, user-scalable=no");
-		else viewport.setAttribute("content", "initial-scale=1, maximum-scale=1, user-scalable=no");
-	}
 }
 
 $(document).ready(load);
