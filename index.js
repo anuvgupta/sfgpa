22e35c3beccafa6902219a5dc3b2c7ca7b6c68fb
Anuv Gupta <anuv.ca@gmail.com>
2016-05-29 04:38:17 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var tools = ['home', 'gpa', 'finals', 'potatoes', 'words', 'easybib'];
 var largeMenu = "<tr><td width = '11%' id = 'l1' class = 'linkTD'><center><img id = 'menuLogo' src = './sfhsOut.png'></center></td> <td width = '17.8%' class = 'linkTD' id = 'l2'> <span class = 'link'>GPA CALCULATOR</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l3'> <span class = 'link'>FINAL GRADES</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l4'> <span class = 'link'>POTATOES</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l5'> <span class = 'link'>WORD COUNTER</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l6'> <span class = 'link'>EASYBIB CITE</span> </td> </tr>";
 var mobileMenu = "<tr height = '17%'> <td width = '26%' class = 'linkTD' id = 'l1'> <center> <img id = 'menuLogo' src = './holycross.png'> <span class = 'link' style = 'display: none;'>HOME</span> </center> </td> <td width = '48%' id = 'lSpacer'> <center> <span class = 'link'>HOME</span> </center> </td> <td width = '26%' class = 'linkTD' id = 'lHamburger'> <center> <img id = 'menuHamburger' src = './hamburger.png'> </center> </td> </tr>";
+var mobileRows = "<tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l2' class = 'linkTD'> <center> <span class = 'link'>GPA CALCULATOR</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l3' class = 'linkTD'> <center> <span class = 'link'>FINAL GRADES</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l4' class = 'linkTD'> <center> <span class = 'link'>POTATOES</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l5' class = 'linkTD'> <center> <span class = 'link'>WORD COUNTER</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l6' class = 'linkTD'> <center> <span class = 'link'>EASYBIB CITE</span> </center> </td> </tr>";
 var menuDown = false;
 var mobile = false;
 
@@ -36,7 +37,7 @@ function setEvents() {
 				$('#menu').css({'transition':'height 0.5s ease-in-out', '-moz-transition':'height 0.5s ease-in-out', '-ms-transition':'height 0.5s ease-in-out', '-webkit-transition':'height 0.5s ease-in-out', '-o-transition':'height 0.5s ease-in-out'});
 				setTimeout(function () { _('menu').style.height = '70%'; }, 1);
 				setTimeout(function () {
-					$('#menuTable tr:last').after("<tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l2' class = 'linkTD'> <center> <span class = 'link'>GPA CALCULATOR</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l3' class = 'linkTD'> <center> <span class = 'link'>FINAL GRADES</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l4' class = 'linkTD'> <center> <span class = 'link'>POTATOES</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l5' class = 'linkTD'> <center> <span class = 'link'>WORD COUNTER</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l6' class = 'linkTD'> <center> <span class = 'link'>EASYBIB CITE</span> </center> </td> </tr>");
+					$('#menuTable tr:last').after(mobileRows);
 					for (var i = 1; i <= tools.length; i++) $('#l' + i).click(lClickHandler(i));
 				}, 10);
 			}
@@ -46,6 +47,7 @@ function setEvents() {
 
 function resize() {
 	if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) {
+		alert('hi');
 		mobile = true;
 		_('menu').style.height = '80px';
 		_('menuTable').innerHTML = mobileMenu;
