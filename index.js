d1b044909455b877875924da8ee52dfa2936c0c6
Anuv Gupta <anuv.ca@gmail.com>
2016-05-29 03:57:14 -0700

    updated mobile index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var tools = ['home', 'gpa', 'finals', 'potatoes', 'words', 'easybib'];
-var windowHeight = window.innerHeight;
+var largeMenu = "<tr><td width = '11%' id = 'l1'><center><img id = 'menuLogo' src = './sfhsOut.png'></center></td> <td width = '17.8%' class = 'linkTD' id = 'l2'> <span class = 'link'>GPA CALCULATOR</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l3'> <span class = 'link'>FINAL GRADES</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l4'> <span class = 'link'>POTATOES</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l5'> <span class = 'link'>WORD COUNTER</span> </td> <td width = '17.8%' class = 'linkTD' id = 'l6'> <span class = 'link'>EASYBIB CITE</span> </td> </tr>";
+var menuDown = false;
 
 function lClickHandler(j) {
 	return function(event) {
@@ -15,9 +16,27 @@ function lClickHandler(j) {
 
 function setEvents() {
 	for (var i = 1; i <= tools.length; i++) $('#l' + i).click(lClickHandler(i));
+	$('#lHamburger').click(function () {
+		if(menuDown) {
+			menuDown = false;
+			$('#menu').css({'transition':'height 0s ease-in-out', '-moz-transition':'height 0s ease-in-out', '-ms-transition':'height 0s ease-in-out', '-webkit-transition':'height 0s ease-in-out', '-o-transition':'height 0s ease-in-out'});
+			for(var i = 5; i > 0; i--) _('menuTable').deleteRow(i);
+			_('menu').style.height = '80px';
+		} else {
+			menuDown = true;
+			$('#menu').css({'transition':'height 0.5s ease-in-out', '-moz-transition':'height 0.5s ease-in-out', '-ms-transition':'height 0.5s ease-in-out', '-webkit-transition':'height 0.5s ease-in-out', '-o-transition':'height 0.5s ease-in-out'});
+			setTimeout(function () { _('menu').style.height = '70%'; }, 1);
+			setTimeout(function () { $('#menuTable tr:last').after("<tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l2' class = 'linkTD'> <center> <span class = 'link'>GPA CALCULATOR</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l3' class = 'linkTD'> <center> <span class = 'link'>FINAL GRADES</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l4' class = 'linkTD'> <center> <span class = 'link'>POTATOES</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l5' class = 'linkTD'> <center> <span class = 'link'>WORD COUNTER</span> </center> </td> </tr> <tr height = '16.6%'> <td width = '88%' colspan = '3' id = 'l6' class = 'linkTD'> <center> <span class = 'link'>EASYBIB CITE</span> </center> </td> </tr>"); }, 10);
+		}
+	});
 }
 
 function resize() {
+	if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) {
+		_('menu').style.height = '80px';
+	} else {
+		_('menu').style.height = '73px';
+	}
 	if (mobile()) {
 		//if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) viewport.setAttribute("content", "initial-scale=1.3, maximum-scale=1.3, user-scalable=no");
 		//else viewport.setAttribute("content", "initial-scale=1, maximum-scale=1, user-scalable=no");
@@ -31,6 +50,7 @@ function load() {
 	});
 	$('head').append("<link rel = 'stylesheet' type = 'text/css' href = './tools/" + tools[0] + "/" + tools[0] + ".css'>");
 	$.getScript('./tools/' + tools[0] + '/' + tools[0] + '.js');
+	resize();
 }
 
 $(window).resize(resize);
