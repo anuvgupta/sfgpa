6d0dc26ac755703a3746214894daeb28349947a9
Anuv Gupta <anuv.ca@gmail.com>
2016-06-24 11:56:59 -0700

    created words project

--- tools/words/words.js
+++ tools/words/words.js
@@ -1,6 +1,18 @@
+var wordsText;
 
 function wordsInit() {
-  
+  wordsText = '';
+  $('#wordsTA').on('change keyup paste', function () {
+    var currentVal = $(this).val();
+    if (currentVal != wordsText) wordsText = currentVal;
+    var wordCount = 0;;
+    for (var i = 0; i < wordsText.length; i++) {
+      if (wordsText[i] == ' ') wordCount++;
+    }
+    _('wordsWordCount').innerHTML = wordCount.toString();
+    _('wordsCharCount').innerHTML = wordsText.length.toString();
+  });
 }
 
 wordsInit();
+//$(document).ready(wordsInit);
