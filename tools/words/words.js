e170f1645ca97b1627a18a7afaffb5dc67d57290
Anuv Gupta <anuv.ca@gmail.com>
2016-06-24 12:26:05 -0700

    updated mobile sizing

--- tools/words/words.js
+++ tools/words/words.js
@@ -16,41 +16,8 @@ function wordsInit() {
 
 function resize() {
 	if ((screen.width < screen.height) || (window.innerWidth < window.innerHeight)) {
-		if (mobile()) {
-			_('gpa').style.fontSize = '32px';
-			_('gpaTitle').style.fontSize = '40px';
-			_('gpa').style.fontSize = '32px';
-			_('gpaText').style.fontSize = '34px';
-			$('.gpaTHead').css('font-size', '21px');
-			$('.gpaA').css('height', '19px');
-			$('.gpaA').css('padding', '0 7px 0 7px');
-			$('.gpaH').css('height', '21px');
-			$('.gpaH').css('padding', '0 7px 0 7px');
-			$('#gpa td').css('font-size', '24px');
-		} else {
-			//alert('small');
-			_('gpa').style.fontSize = '36px';
-			_('gpaTitle').style.fontSize = '46px';
-			_('gpa').style.fontSize = '36px';
-			_('gpaText').style.fontSize = '38px';
-			$('.gpaTHead').css('font-size', '25px');
-			$('.gpaA').css('height', '23px');
-			$('.gpaA').css('padding', '0 13px 0 13px');
-			$('.gpaH').css('height', '25px');
-			$('.gpaH').css('padding', '0 13px 0 13px');
-			$('#gpa td').css('font-size', '36px');
-		}
-	} else {
-		_('gpa').style.fontSize = '43px';
-		_('gpaTitle').style.fontSize = '53px';
-		_('gpa').style.fontSize = '43px';
-		_('gpaText').style.fontSize = '45px';
-		$('.gpaTHead').css('font-size', '32px');
-		$('.gpaA').css('height', '30px');
-		$('.gpaA').css('padding', '0 20px 0 20px');
-		$('.gpaH').css('height', '32px');
-		$('.gpaH').css('padding', '0 20px 0 20px');
-		$('#gpa td').css('font-size', '43px');
+    _('wordsTitle').style.fontSize = '47px';
+    _('wordsSmall').style.fontSize = '12px';
 	}
 }
 
